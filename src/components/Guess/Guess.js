96c90aa2799683537441b481727c052cba9543e4
Jack Murra <jack.murra@gmail.com>
2023-09-20 21:41:04 -0500

    exercise 1

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -14,9 +14,12 @@ function Guess() {
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
         value={guess}
+        minLength={5}
+        maxLength={5}
         pattern="[a-zA-Z]{5}"
         title="5 letter word"
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
