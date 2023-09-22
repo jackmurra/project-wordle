49070d5c37461e95c975c2e52f1bb5202746e6c1
Jack Murra <jack.murra@gmail.com>
2023-09-21 21:51:27 -0500

    exercise 3

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import Guess from "../Guess";
 import GuessResults from "../GuessResults";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,16 +13,20 @@ console.info({ answer });
 
 function Game() {
   const [guessResults, setGuessResults] = React.useState([]);
-  const addGuessResult = guess => {
-      const newGuess = {id: crypto.randomUUID(), label: guess}
-      const nextGuessResults = [...guessResults, newGuess]
-      setGuessResults(nextGuessResults)
-  }
+  const addGuessResult = (guess) => {
+    const newGuess = { id: crypto.randomUUID(), label: guess };
+    const nextGuessResults = [...guessResults, newGuess];
+    setGuessResults(nextGuessResults);
+  };
+
+  console.log('guessResults',guessResults.length)
 
   return (
     <>
       <GuessResults guessResults={guessResults} />
-      <Guess addGuessResult={addGuessResult} />
+      {NUM_OF_GUESSES_ALLOWED > guessResults.length && (
+        <Guess addGuessResult={addGuessResult} />
+      )}
     </>
   );
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { range } from "../../utils";
 
 function GuessResults({ guessResults }) {
+  const emptyRows = range(6 - guessResults.length);
+  const whitespaceRows = emptyRows.map(() => ({
+    id: crypto.randomUUID(),
+    label: "     ",
+  }));
+  const rows = [...guessResults, ...whitespaceRows];
+
   return (
     <div className="guess-results">
-      {guessResults.map(({ id, label }) => (
+      {rows.map(({ id, label }) => (
         <p key={id} className="guess">
-          {label}
+          {label.split("").map((character, index) => (
+            <span key={index} className="cell">
+              {character}
+            </span>
+          ))}
         </p>
       ))}
     </div>
